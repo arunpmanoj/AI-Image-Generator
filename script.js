e55f500889e6d740f9f391f9f26b063a0f4f191f
arun <arunpmanoj12@gmail.com>
2025-07-06 18:29:19 +0530

    added the api key and make the page responsive

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const themeToggle = document.querySelector(".theme-toggle");
 const promptBtn = document.querySelector(".prompt-btn");
+const generateBtn = document.querySelector(".generate-btn");
 const promptInput = document.querySelector(".prompt-input");
 const promptForm = document.querySelector(".prompt-form");
 const modelSelect = document.getElementById("model-select");
 const ratioSelect = document.getElementById("ratio-select");
 const countSelect = document.getElementById("count-select");
 const gridGallery = document.querySelector(".gallery-grid");
-
-
+const API_KEY = "" //HUGGING FACE API_KEY;
 
 const examplePrompts = [
     "A magic forest with glowing plants and fairy homes among giant mushrooms",
@@ -41,9 +41,74 @@ const toggleTheme = () => {
     themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
 }
 
-const createImageCards = (selectedModel, imageCount, aspectRatio, promptText)=>{
-    for (let i=0; i<imageCount;i++){
-        gridGallery.innerHTML ="";
+const getImageDimensions = (aspectRatio, baseSize = 512) => {
+    const [width, height] = aspectRatio.split("/").map(Number);
+    const scaleFactor = baseSize / Math.sqrt(width * height);
+
+    let calculatedWidth = Math.round(width * scaleFactor);
+    let calculatedHeight = Math.round(height * scaleFactor);
+    //AI_Model Requirements
+    calculatedWidth = Math.floor(calculatedWidth / 16) * 16;
+    calculatedHeight = Math.floor(calculatedHeight / 16) * 16;
+
+    return {
+        width: calculatedWidth, height: calculatedHeight
+    };
+}
+
+const updateImageCard = (imgIndex, imgUrl) => {
+    const imgCard = document.getElementById(`img-card-${imgIndex}`);
+    if (!imgCard) return;
+
+    imgCard.classList.remove("loading");
+    imgCard.innerHTML = `<img src="${imgUrl}" class="result-img">
+                        <div class="img-overlay">
+                            <a href="${imgUrl}" class="img-download-btn" download="${Date.now()}.png">
+                                <i class="fa-solid fa-download"></i>
+                            </a>
+                        </div>`
+
+}
+
+const generateImages = async (selectedModel, imageCount, aspectRatio, promptText) => {
+    const MODEL_URL = `https://api-inference.huggingface.co/models/${selectedModel}`;
+    const { width, height } = getImageDimensions(aspectRatio);
+    generateBtn.setAttribute("disabled","true");
+
+    const imagePromises = Array.from({ length: imageCount }, async (_, i) => {
+        try {
+            const response = await fetch(MODEL_URL, {
+                headers: {
+                    Authorization: `Bearer ${API_KEY}`,
+                    "Content-Type": "application/json",
+                    "x-use-cache": "false",
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    inputs: promptText,
+                    parameters: { width, height },
+                    options: { wait_for_model: true, user_cache: false },
+                }),
+            });
+
+            if (!response.ok) throw new Error((await response.json())?.error);
+
+            const result = await response.blob();
+            updateImageCard(i, URL.createObjectURL(result));
+        }
+        catch (error) {
+            console.log(error);
+            const imgCard = document.getElementById(`img-card-${i}`);
+            imgCard.classList.replace("loading","error");
+            imgCard.querySelector(".status-text").textContent = "Generation failed! check console for more details"
+        }
+    })
+    await Promise.allSettled(imagePromises);
+    generateBtn.removeAttribute("disabled");
+}
+
+const createImageCards = (selectedModel, imageCount, aspectRatio, promptText) => {
+    for (let i = 0; i < imageCount; i++) {
         gridGallery.innerHTML += `<div class="img-card loading" id="img-card-${i}" style="aspect-ratio:${aspectRatio}">
                         <div class="status-container">
                             <div class="spinner"></div>
@@ -52,7 +117,8 @@ const createImageCards = (selectedModel, imageCount, aspectRatio, promptText)=>{
                         </div>
                     </div>`;
     }
-}
+    generateImages(selectedModel, imageCount, aspectRatio, promptText);
+};
 
 const handleFormSubmit = (e) => {
     e.preventDefault();
